93211ecdf85c94a691bc48ac991eeb7821ef0eec
Sasha <sasham_c@yahoo.com>
2021-12-27 14:08:52 -0600

    more debuggin. I believe my data boxes were successfully created

--- func.js
+++ func.js
@@ -1,5 +1,4 @@
 //Create your variables prior for easier access
-const gameBoard = document.querySelectorAll('.box');
 const newGameButton = document.querySelector('#new_game');
 const helpButton = document.querySelector('#help');
 const scoreBoard = document.querySelector('#score_board');
@@ -7,30 +6,32 @@ const finalMessage = document.querySelector('.final_message');
 
 
 // Create the Board
-let gameGrid = class boxValues {
+class boxValues {
     constructor(boxes) {
         this.boxes = {
             boxes:Array(9).fill(null),
             xIsNext: true,
         }
     }
-playersChoice(i){
-    const boxes = this.boxes.boxes;
-    boxes[i] = this.boxes.xIsNext ? 'X':'O';
-    this.holdPlace({
-        boxes: boxes,
-        xIsNext: !this.boxes.xIsNext,
-    });
-}   
+    playersChoice(i){
+        const boxes = this.boxes.boxes;
+        boxes[i] = this.boxes.xIsNext ? 'X':'O';
+        this.holdPlace({
+            boxes: boxes,
+            xIsNext: !this.boxes.xIsNext,
+        });
+    }   
 }
-console.log(gameGrid);
+const gameBoard = new boxValues();
+
+console.log(gameBoard);
 
 
 
-for(let i=0; i<9; i++){
-    let div = document.createElement('div');
-    div.setAttribute('class', 'box');
-    // document.getElementById('[#game_board]').appendChild(div);
-}
-let box = document.querySelectorAll('.box');
-console.log(box);
+// for(let i=0; i<9; i++){
+//     let div = document.createElement('div');
+//     div.setAttribute('class', 'box');
+//     // document.getElementById('[#game_board]').appendChild(div);
+// }
+// let box = document.querySelectorAll('.box');
+// console.log(box);
