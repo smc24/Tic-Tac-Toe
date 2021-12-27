8a93337d90c13554a4c30c43c8e3b76a0dedc1b9
Sasha <sasham_c@yahoo.com>
2021-12-26 21:11:36 -0600

    created a game variable with the value of a class

--- func.js
+++ func.js
@@ -4,3 +4,14 @@ const newGameButton = document.querySelector('#new_game');
 const helpButton = document.querySelector('#help');
 const scoreBoard = document.querySelector('#score_board');
 const finalMessage = document.querySelector('.final_message');
+
+
+// Create the Board
+let gameGrid = class Board {
+    constructor(boxes) {
+        this.boxes = {
+            boxes:Array(9).fill(null),
+            xIsNext: true,
+        }
+    }
+}
