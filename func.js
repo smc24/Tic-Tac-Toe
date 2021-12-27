fe49da57b11d02432ba5555906167fc635d3413b
Sasha <sasham_c@yahoo.com>
2021-12-26 21:02:16 -0600

    made some variables

---
+++ func.js
@@ -0,0 +1,6 @@
+//Create your variables prior for easier access
+const gameBoard = document.querySelectorAll('.box');
+const newGameButton = document.querySelector('#new_game');
+const helpButton = document.querySelector('#help');
+const scoreBoard = document.querySelector('#score_board');
+const finalMessage = document.querySelector('.final_message');
