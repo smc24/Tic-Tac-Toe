ab10d540333eccdc212c549309004270b6a65789
Sasha <sasham_c@yahoo.com>
2021-12-29 12:24:54 -0600

    game is properly functioning. final touches on css. readme created.

--- func.js
+++ func.js
@@ -1,13 +1,11 @@
 //Create your variables prior for easier access
 const newGameButton = document.querySelector('#new_game');
 const helpButton = document.querySelector('#help');
-// const scoreBoard = document.querySelector('#score_board');
-const finalMessage = document.querySelector('#final_message');
 const winningCombinations = [
     [0,1,2], //rows
     [3,4,5],
     [6,7,8],
-    [0,3,6], //colomns
+    [0,3,6], //columns
     [1,4,7],
     [3,5,8],
     [0,4,8], //diagonals
@@ -19,6 +17,7 @@ class BoxValues {
     constructor(boxes) {
         this.boxes = boxes;
         this.xIsNext = true; // X goes first
+        console.log(this.boxes)
     }
     playersChoice(element){
         const boxes = this.boxes;
@@ -31,11 +30,8 @@ class BoxValues {
     //Web Dev Simplified YT source
     whoWins(currentCharacter){
         const boxElements = this.boxes;
-        // console.log(currentCharacter);//make sure it's displaying the character alone
         return winningCombinations.some((combo)=> { //Checks to see if at least one combination is true
-            // console.log(combo);
             return combo.every((index)=> { // iterates through through the combo to find 3 in a row
-                // console.log(index);
                 return boxElements[index].innerText===currentCharacter; //grabs value at the given index
                 
             }
@@ -43,79 +39,56 @@ class BoxValues {
         })
     }
 
-    //now to end the game. At this point the user should no longer be able to play and the final message should be displayed.
-    letsEndThis(verdict){
-       if(verdict==='X'){
-
-       } else {
-           alert(`Ayyee Congrats ${this.xIsNext ? 'X':'O'}! You win!`);
-       }
-   }
-
-   newGame(){
-       console.log(BoxValues.boxes);
-       let defaultValues = this.boxes;
-       return BoxValues.boxes;
-   }
+    
 }
    
    
    
 //Connect to div.box elements 
 let gameBoard = new BoxValues(document.querySelectorAll('.box'));
-// console.log(gameBoard.boxes);
 
 
+function eventHandler(event){
+    console.log(event.target);
+        
+    //box can only be clicked on once
+    if(event.target.innerText==='') {//ASK ABOUT THE EMPTY STRING
+        gameBoard.playersChoice(event.target)//just the box
+    }
+   
+    //added to keep track of values for winning combinations
+    //need to display winning message
+    if(gameBoard.whoWins(event.target.innerText)){
+        console.log('winner');
+        setTimeout(()=>{ //allows the mark to be placed before the winning message
+            alert(`Ayyee Congrats ${gameBoard.xIsNext ? 'O':'X'}! You win!`);
+            },500); 
+            gameBoard.boxes.forEach((box)=> {
+                box.removeEventListener('click', eventHandler);
+        })
+    } else {
+            console.log('Tie');
+    }
+}
 //Let's get the event listeners going
 gameBoard.boxes.forEach((box)=> {
-    box.addEventListener('click', (event)=> {
-        console.log(event.target);
-        
-        //box can only be clicked on once
-        if(event.target.innerText==='') {//ASK ABOUT THE EMPTY STRING
-            gameBoard.playersChoice(event.target)//just the box
-        }
-       
-        //added to keep track of values for winning combinations
-        //need to display winning message
-        if(gameBoard.whoWins(event.target.innerText)){
-            
-            // box.removeEventListener('click', (event)=> { //possible way to end the game
-            //     console.log(event.target);
-            //     if(event.target.innerText==='') {
-                //         gameBoard.playersChoice(event.target)
-                //     }
-                // }, false)
-                
-                gameBoard.letsEndThis(event.target);
-                console.log('winner');
-            } else {
-                console.log('Tie')
-                
-            } 
-            
-        });
-        
+    box.addEventListener('click', eventHandler)     
     });
+   
+
     
-    //append child to finalMessage html element
-    let winningMessageTxt = document.createElement('div');
-    winningMessageTxt.setAttribute('id', 'txt');
-    finalMessage.appendChild(winningMessageTxt);
-    
-    //create a variable with the value of the final message element text
-    let winningTxt = document.querySelector('#txt').innerText;
-    console.log(winningTxt); //just to validate
-    
-    //let's get those buttons percolatin'
-    function newGame(){
-        console.log('working');
-        return this.boxes;
-    }
-    newGameButton.addEventListener('click', gameBoard.newGame());
+//let's get those buttons percolatin'
+   newGameButton.addEventListener('click', ()=> {
+       console.log('new game')
+       gameBoard = new BoxValues(document.querySelectorAll('.box')); //reassigning the value of the gameBoard variable in this case works 
+       gameBoard.boxes.forEach((box)=> { //ends the game
+           box.innerText = '';
+           box.addEventListener('click', eventHandler) //adds event listener back when user starts a new game  
+       })
+   });
     
     function instructions(){
-        alert("No worries! Try and get three in a row before your opponent. If your opponent has two in a row, mark the third spot before they do! Hint: Figure out the pattern and you'll never lose");
+        alert("You got this! Try and get three in a row before your opponent. If your opponent has two in a row, mark the third spot before they do! Tip: Figure out the pattern and you'll never lose");
     }
     
     helpButton.addEventListener('click', instructions);
